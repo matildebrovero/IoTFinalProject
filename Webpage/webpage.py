1754e9959234a879c2a3d790cd114b81a6dab26a
Matilde Brovero <mati.brovero@gmail.com>
2024-03-18 22:40:54 +0100

    add "Add Patient" section

--- Webpage/webpage.py
+++ Webpage/webpage.py
@@ -31,5 +31,26 @@ def getData():
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)})
 
+@app.route('/addPatient', methods=['POST'])
+def add_patient():
+    new_patient_data = request.form.to_dict()
+    # Salvare i dati del nuovo paziente o fare altro con essi
+    print("New Patient Data:", new_patient_data)
+
+    #TODO: cambiare l'indirizzo in base al server che ospita il servizio - LEGGERE DA FILE JSON DI CONFIGURAZIONE
+    uri = f"http://localhost:8080/patient"
+    print(uri)
+
+    try:
+        # salvo i pazienti nel server
+        response = requests.post(uri, data=new_patient_data)
+        print(response)
+        response.raise_for_status()  # Raise an exception if the request was unsuccessful
+        data = response.json()
+        print(data)
+        return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)})
+
 if __name__ == '__main__':
     app.run(debug=True)
