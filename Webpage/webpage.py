d58d454ded19f95658131ae2f48df0032da1e061
Matilde Brovero <mati.brovero@gmail.com>
2024-03-23 19:12:45 +0100

    added possibility to delete a patient from the webpage

--- Webpage/webpage.py
+++ Webpage/webpage.py
@@ -18,11 +18,11 @@ def index():
     urlCatalog = conf["RegistrySystem"]
     print(urlCatalog)
     # read information from the configuration file and POST the information to the catalog
-    config = conf["information"]
-    print(config)
-    config = requests.post(f"{urlCatalog}/service", data=config)
-    conf["information"] = config.json()
-    save_config(conf)
+    #config = conf["information"]
+    #print(config)
+    #config = requests.post(f"{urlCatalog}/service", data=config)
+    #conf["information"] = config.json()
+    #save_config(conf)
     
     uri = f"{urlCatalog}/configwebpage"
     print(uri)
@@ -48,7 +48,7 @@ def getData():
     conf = read_config()
 
     # URI to ask data from the database for a certain patient and time range
-    uri = f"{conf["Database"]}/{dataSelected}/{patientSelected}?range={timeRange}"
+    uri = f"{conf['Database']}/{dataSelected}/{patientSelected}?range={timeRange}"
     print(uri)
 
     try:
@@ -68,12 +68,34 @@ def add_patient():
     #print("New Patient Data:", new_patient_data)
     conf = read_config()
     # URI to post data in the catalog
-    uri = f"{conf["RegistrySystem"]}/patient"
+    uri = f"{conf['RegistrySystem']}/patient"
     print(uri)
 
     try:
         # Save the new patient data in the catalog
         response = requests.post(uri, data=new_patient_data)
+        print(response)
+        response.raise_for_status()  # Raise an exception if the request was unsuccessful
+        data = response.json()
+        print(data)
+        return jsonify(data)
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)})
+
+@app.route('/deletePatient', methods=['POST'])
+def delete_patient():
+    # Get the data from the form
+    patient_id = request.form.to_dict()
+    patient_id = patient_id['selectedpatient']
+    #print("Patient ID:", patient_id)
+    conf = read_config()
+    # URI to delete data in the catalog
+    uri = f"{conf['RegistrySystem']}/patient/{patient_id}"
+    print(uri)
+
+    try:
+        # Delete the patient data in the catalog
+        response = requests.delete(uri)
         print(response)
         response.raise_for_status()  # Raise an exception if the request was unsuccessful
         data = response.json()
