d27864550668ddb7906a0fb98384bbc3a44ddd91
nittifra <s310611@studenti.polito.it>
2024-04-09 23:09:10 +0200

    Definitive

--- ECGAnalysis/ECG_Analysis.py
+++ ECGAnalysis/ECG_Analysis.py
@@ -10,17 +10,22 @@ import scipy
 class ECGAnalysis:
 
     """
-    ECGAnalysis - SmartHospital IoT platform. Version 1.0.2
-    This microservice is responsible for analyzing the ECG data and publishing the results to the Database Connector.
-    The results are published according to the SenML format.
+    ECGAnalysis - SmartHospital IoT platform. Version 1.0.3
+    This microservice is responsible for processing the raw ECG data coming from the Device Connector.
+    The signal is filtered, the RR wave is detected and the heart rate is calculated. 
+    The results are published according to the SenML format. 
     
         Input: 
             - ECG data from device connector.
 
         Output: 
             - Heart Rate
-            - Filtered ECG signal
             - RR wave
+
+            - Filtered ECG signal
+              DISCLAIMER:
+              Dealing with a one derivation ECG, each minute only a snippet is provided of the whole 60s signal to avoid waste of resources,
+              however the morphology of the ECG signal is guaranteed and kept in the snippet. 
 
     --------------------------------------------------------------------------
     --------------         standard configuration          -------------------
@@ -36,12 +41,12 @@ class ECGAnalysis:
             - "serviceID": ID of the service, automatically assigned by the Registry System 
 
             - "subscribe_topic": Where the ECG data are expected to come in input. Data are taken in with a MQTT Wildcard +,
-                        Example: SmartHospitalN/*** PatientN ***/ECG
-                            The data are then re-published in the "publish_topic" topic, with the *** PatientN *** information
+                        Example: SmartHospitalN/*** patientN ***/ECG
+                            The data are then re-published in the "publish_topic" topic, with the *** patientN *** information
                             filled with the actual patient data. 
 
             - "publish_topic": Publish topic of the service, the main topic where the "analysis" will be published
-                        Example: SmartHospitalN/ *** publish_topic ***/PatientN / ***analysis_1***
+                        Example: SmartHospitalN/ *** publish_topic ***/patientN/ ***analysis_1***
 
             
             - "analysis": List of analysis to be performed. Those are the topics where the different
@@ -86,19 +91,22 @@ class ECGAnalysis:
 
     def notify(self, topic, payload):
 
+        # Define the topics where the different analysis will be published
+
         topic_parts = topic.split("/")
-        print(topic_parts)
 
         patient_id = topic_parts[2]
         base_topic = topic_parts[0]
-        print(base_topic)
 
         topic_pub = f"{base_topic}/{self.servicepub}/{patient_id}"
 
         message_json = json.loads(payload)
         data = message_json["e"]
         ecg_data = np.array([entry['v'] for entry in data])
+
+        # The data are temporized according to the original timestamp of the raw signal.
         basetime = message_json["bt"]
+
 
         topic_pubs = [f"{topic_pub}/{self.analysis[0]}",f"{topic_pub}/{self.analysis[1]}",f"{topic_pub}/{self.analysis[2]}"]
 
@@ -114,17 +122,17 @@ class ECGAnalysis:
         filtered = out["filtered"]
         rr_wave = np.diff(out["rpeaks"]/fc)
 
-        # Resample ECGdata to the desired frequency (e.g., 40Hz), to keep morphology and minimize data.
+        # Resample ECGdata from "sampling_frequency" to "sample_duration" to minimize data while keeping morphology.
         resampled_ecg_data = scipy.signal.resample(filtered, int(len(filtered) * self.target_frequency / self.fc))
 
-        # Take the first 10 seconds of the resampled signal
+        # Take the first "sample_duration" seconds of the resampled signal
         snippet_length = min(len(resampled_ecg_data), self.sample_duration * self.target_frequency)
         snippet_ecg_data = resampled_ecg_data[:snippet_length]
 
         # Adjust timestamps for the snippet
         snippet_timestamps = np.linspace(0, self.sample_duration, len(snippet_ecg_data), endpoint=False)
 
-        # ECG filtered output for the snippet
+        # ECG filtered output 
         filtered_data = []
         for i, value in enumerate(snippet_ecg_data):
             entry = {
@@ -167,19 +175,16 @@ class ECGAnalysis:
             ]
         }   
 
-
         self.publish(heart_rate_output, topic_pubs[2])
-        print(f"\nheart rate: {heart_rate} bpm\n")
 
         self.publish(rr_output, topic_pubs[1])
-        print(f"\nRR wave: {rr_wave} ms\n")
 
         self.publish(filtered_output, topic_pubs[0])
 
 
     def publish(self, output, topic_pub):
         self.ClientPublisher.myPublish(topic_pub, output)
-        return print("published")
+        return print(f"{topic_pub} published. \n")
         
 
     def startSim(self):
@@ -213,7 +218,7 @@ if __name__ == "__main__":
     request = requests.get(f"{RegistrySystem}/{conf['information']['uri']['broker_info']}") #get the broker info
     MQTTinfo = json.loads(request.text) 
 
-    # Define the topics and the MQTT clientID, with the new configuration got from the RegistrySystem
+    # Load the information necessary for the instantiation of the ECGAnalysis microservice
     
     servicepub = conf["information"]["publish_topic"]
     clientID_sub = conf["information"]["serviceName"] + str(conf["information"]["serviceID"]) + "_sub"
@@ -223,12 +228,13 @@ if __name__ == "__main__":
     analysis = conf["information"]["analysis"]
     topic_sub =  MQTTinfo["main_topic"] + conf["information"]["subscribe_topic"]
 
-    # The sampling frequency of the ECG is a project choice, so it is defined considering that all physical device will have the same frequency.
-
     fc = conf["information"]["sampling_frequency"] 
     target_frequency = conf["information"]["target_frequency"]
     sample_duration = conf["information"]["sample_duration"]
+
+
     # Create an instance of ECGAnalysis
+
     myECGAnalysis = ECGAnalysis(clientID_sub, clientID_pub, broker, port, topic_sub, fc, servicepub, analysis, target_frequency, sample_duration)
     myECGAnalysis.startSim()
     start_time = time.time()
@@ -237,20 +243,20 @@ if __name__ == "__main__":
         while True:
             current_time = time.time()
             if current_time - start_time > 5*60:
+                # After 5 minutes we update the information about the service
                 config_file = json.load(open('ECGAn_configuration.json'))
                 config = requests.put(f"{RegistrySystem}/{conf['information']['uri']['add_service']}", json=config_file["information"])
                 if config.status_code == 200:
-                    print(f"information: {config}")
                     config_file["information"] = config.json()
                     # print the updated information about the service
-                    print(f"information: {config_file['information']}")
+                    print(f"\nInformation updated: {config_file['information']}\n")
                     json.dump(config_file, open("ECGAn_configuration.json", "w"), indent = 4)
                     start_time = current_time
                 else:
                     print(f"Error: {config.status_code} - {config.text}")
             else:
                 pass
-            time.sleep(2)
+            #time.sleep(1)
     except KeyboardInterrupt:
         myECGAnalysis.StopSim()
         
